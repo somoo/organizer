cc367ea5eb267269369f5f752c0dcf87d9cc3ddd
Benjamin Böhm <benni@fs.lmu.de>
2018-07-10 20:13:56 +0200

    [first and minor change from b]

--- src/main/java/com/somoo/organizer/OrganizerUI.java
+++ src/main/java/com/somoo/organizer/OrganizerUI.java
@@ -32,7 +32,7 @@ public class OrganizerUI extends UI {
         Button button = new Button("Click Me");
         button.addClickListener( e -> {
             layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
+                    + ", it works great!"));
         });
         
         layout.addComponents(name, button);
